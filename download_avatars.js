cb9c3dd025bc1272dc76a8ec477e9f01827d2af2
Rafael <rafaelmarianorocha@gmail.com>
2018-07-10 00:54:21 +0000

    Completed Step8 downloader.

--- download_avatars.js
+++ download_avatars.js
@@ -1,56 +1,55 @@
 var request = require('request');
 var token = require('./secrets.js');
 var fs = require('fs');
+var args = process.argv;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': token.GITHUB_TOKEN
-    }
+  if (args.length !== 4){
+    throw Error;
+  }
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': token.GITHUB_TOKEN
+        }
 
-  };
+    };
 
-  request(options, function(err, res, body) {
-    var repos = JSON.parse(body);
-    repos.forEach(function(repo){
-      console.log(repo.avatar_url);
-    })
-  });
+
+    request(options, function(err, res, body) {
+        cb(err, body);
+    });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors(args[2], args[3], function(err, result) {
+    var repos = JSON.parse(result);
+    repos.forEach(function(repo){
+    var login = repo.login;
+    var url = repo.avatar_url;
+    downloadImageByURL(url, login);
+  });
 });
 
 function downloadImageByURL(url, filePath) {
 
-request.get(url)               // Note 1
-       .on('error', function (err) {                                   // Note 2
-         throw err;
-       })
-       .on('response', function (response) {
-        if (response.statusCode < 200 || response.statusCode >= 300){
-          console.log("Error " + response.statusCode + " found.");
-          throw Error();
-        }
-         console.log('Response Status Code: ', response.statusCode);
-         console.log('Response message: ' + response.statusMessage);
-         console.log('Content type: ' + response.headers['content-type']);
-       })
-       .on('end', function(){
-        console.log('Download complete.');
-       })
-       .pipe(fs.createWriteStream(filePath));
+    request.get(url) // Note 1
+        .on('error', function(err) { // Note 2
+            throw err;
+        })
+        .on('response', function(response) {
+            if (response.statusCode < 200 || response.statusCode >= 300) {
+                console.log("Error " + response.statusCode + " found.");
+                throw Error();
+            }
+        })
+        .on('end', function() {
+            console.log('Download complete.');
+        })
+        .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
 
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg");
-
-
-
-
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg");
